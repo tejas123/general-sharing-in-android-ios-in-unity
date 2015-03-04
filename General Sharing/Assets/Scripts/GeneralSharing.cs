d8f0060dc51a47513ad5959dda9227bb5139eec3
Bharat <bharatappguru@gmail.com>
2015-03-04 17:14:52 +0530

    Latest updated

--- General Sharing/Assets/Scripts/GeneralSharing.cs
+++ General Sharing/Assets/Scripts/GeneralSharing.cs
@@ -31,32 +31,43 @@ public class GeneralSharing : MonoBehaviour {
 
 	public void OnAndroidTextSharingClick()
 	{
-		#if UNITY_ANDROID
-		// Create Refernece of AndroidJavaClass class for intent
-		AndroidJavaClass intentClass = new AndroidJavaClass("android.content.Intent");
-		// Create Refernece of AndroidJavaObject class intent
-		AndroidJavaObject intentObject = new AndroidJavaObject("android.content.Intent");
 
-		// Set action for intent
-		intentObject.Call<AndroidJavaObject>("setAction", intentClass.GetStatic<string>("ACTION_SEND"));
-		//Set Subject of action
-		intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_SUBJECT"), "Text Sharing ");
-		//Set title of action or intent
-		intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_TITLE"), "Text Sharing ");
-		// Set actual data which you want to share
-		intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_TEXT"), "Text Sharing Android Demo");
-
-
-		AndroidJavaClass unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-		AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject>("currentActivity");
-		// Invoke android activity for passing intent to share data
-		currentActivity.Call("startActivity", intentObject);
-
-		
-		#endif
-		
+		StartCoroutine(ShareAndroidText());
 
 	}
+	IEnumerator ShareAndroidText()
+	{
+		yield return new WaitForEndOfFrame();
+#if UNITY_ANDROID
+		byte[] bytes = MyImage.EncodeToPNG();
+		string path = Application.persistentDataPath + "/MyImage.png";
+		File.WriteAllBytes(path, bytes);
+
+		AndroidJavaClass intentClass = new AndroidJavaClass("android.content.Intent");
+		AndroidJavaObject intentObject = new AndroidJavaObject("android.content.Intent");
+
+		intentObject.Call<AndroidJavaObject>("setAction", intentClass.GetStatic<string>("ACTION_SEND"));
+		intentObject.Call<AndroidJavaObject>("setType", "image/*");
+		intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_SUBJECT"), "Text Sharing ");
+		intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_TITLE"), "Text Sharing ");
+		intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_TEXT"), "Text Sharing Android Demo");
+
+		AndroidJavaClass uriClass = new AndroidJavaClass("android.net.Uri");
+
+		AndroidJavaObject fileObject = new AndroidJavaObject("java.io.File", path);// Set Image Path Here
+
+		AndroidJavaObject uriObject = uriClass.CallStatic<AndroidJavaObject>("fromFile", fileObject);
+
+		//			string uriPath =  uriObject.Call<string>("getPath");
+		bool fileExist = fileObject.Call<bool>("exists");
+		Debug.Log("File exist : " + fileExist);
+		AndroidJavaClass unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+		AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject>("currentActivity");
+		currentActivity.Call("startActivity", intentObject);
+
+#endif
+	}
+
 
 	public void OnAndroidMediaSharingClick()
 	{
